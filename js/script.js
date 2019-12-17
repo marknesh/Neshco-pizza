45e14aa239463845f7feefc0806aa620d1df5eef
marknesh <markmunene.nesh@gmail.com>
2019-12-17 10:02:22 +0300

    create function to add total

--- js/script.js
+++ js/script.js
@@ -54,6 +54,7 @@ $(".showCrust").text(crustPrice)
 var beef=parseInt($("input[name='topp']:checked").val())
 var pineapple=parseInt($("input[name='top']:checked").val())
 var beacon=parseInt($("input[name='to']:checked").val())
+var cheese=parseInt($("input[name='cheese']:checked").val())
 
 
 
@@ -62,6 +63,9 @@ var beacon=parseInt($("input[name='to']:checked").val())
 
 var delivery=parseInt($(".delivered :checked").val())
 $(".deliveryFee").text(delivery)
+
+if($("input#yes").is(':checked')){var sasa=prompt("Please enter your location")&&alert("Your delicious pizza will be delivered to you shortly after you checkout")}
+
 
 
 var numberPizza=parseInt($("#total").val())
@@ -72,17 +76,13 @@ $(".showBeacon").text(beacon)
 
 
 
- if(beef==40 && pineapple==50){
-  $(".showBeacon").hide()
-  $(".titleBeacon").hide()
-  $(".total").text((sizePizza+crustPrice+beef+pineapple+delivery)*numberPizza)
-
-}
 var mbeef=(beef+20)
 
 var mpineapple=(pineapple+20)
 
 var mbeacon=(beacon+20)
+
+var mcheese=(cheese+20)
 
 
 
@@ -90,6 +90,18 @@ var lbeef=(beef+30)
 var lpineapple=(pineapple+30)
 
 var lbeacon=(beacon+30)
+var lcheese=(cheese+30)
+
+
+alert(cheese)
+
+if(sizePizza==500)
+{$(".showBeef").text(beef)
+$(".showBeacon").text(beacon)
+$(".showPineapple").text(pineapple)
+$(".showCheese").text(cheese)
+
+}
 
 
 
@@ -97,78 +109,114 @@ var lbeacon=(beacon+30)
 if(sizePizza==1500){
   $(".showBeef").text(mbeef)
   $(".showPineapple").text(mpineapple)
-  $(".showBeacon").text(mbeacon)}
-  if(mbeef==60 && mpineapple==70){
+  $(".showBeacon").text(mbeacon)
+  $(".showCheese").text(mcheese)}
+  if(lbeef==70 && lpineapple==80){
     $(".showBeacon").hide()
     $(".titleBeacon").hide()
-    $(".total").text((sizePizza+crustPrice+mbeef+mpineapple+delivery)*numberPizza)
+    $(".total").text((sizePizza+crustPrice+lbeef+lcheese+lpineapple+delivery)*numberPizza);
+  
+  }
+ if(mbeef==60 && mpineapple==70 ){
+    $(".showBeacon").hide()
+    $(".titleBeacon").hide()
+    $(".total").text((sizePizza+crustPrice+mbeef+mcheese+mpineapple+delivery)*numberPizza);
+    
+  
+
+ if(beef==40 && pineapple==50){
+  $(".showBeacon").hide()
+  $(".titleBeacon").hide()
+  $(".total").text((sizePizza+crustPrice+beef+pineapple+delivery)*numberPizza);
+
+}
   
   }
   if(sizePizza==2000){
     $(".showBeef").text(lbeef)
     $(".showPineapple").text(lpineapple)
     $(".showBeacon").text(lbeacon)
+    $(".showCheese").text(lcheese)
   
   }
   if(lbeef==70 && lpineapple==80){
     $(".showBeacon").hide()
     $(".titleBeacon").hide()
-    $(".total").text((sizePizza+crustPrice+lbeef+lpineapple+delivery)*numberPizza);return
+    $(".total").text((sizePizza+crustPrice+lbeef+lcheese+lpineapple+delivery)*numberPizza);
   
   }
 
 if(beef==40 && pineapple==50){
   $(".showBeacon").hide()
   $(".titleBeacon").hide()
-  $(".total").text((sizePizza+crustPrice+beef+pineapple+delivery)*numberPizza)
+  $(".total").text((sizePizza+crustPrice+beef+cheese+pineapple+delivery)*numberPizza);return
 
 }
 
  if(beef==40&&beacon==60){
   $(".showPineapple").hide()
   $(".titlePineapple").hide()
-  $(".total").text((sizePizza+crustPrice+beef+beacon+delivery)*numberPizza)
+  $(".total").text((sizePizza+crustPrice+cheese+beef+beacon+delivery)*numberPizza);
 
 }
 if(mbeef==60&&mbeacon==80){
   $(".showPineapple").hide()
   $(".titlePineapple").hide()
-  $(".total").text((sizePizza+crustPrice+mbeef+mbeacon+delivery)*numberPizza)
+  $(".total").text((sizePizza+crustPrice+mbeef+mcheese+mbeacon+delivery)*numberPizza);
 
 }
 if(lbeef==70 && lbeacon==90){
   $(".showPineapple").hide()
   $(".titlePineapple").hide()
-  $(".total").text((sizePizza+crustPrice+lbeef+lbeacon+delivery)*numberPizza)
+  $(".total").text((sizePizza+crustPrice+lbeef+lcheese+lbeacon+delivery)*numberPizza);
 
 }
-if(beacon==60&&pineapple==50)
+if(beacon==60 && pineapple==50)
 {
   $(".showBeef").hide()
   $(".titleBeef").hide()
-  $(".total").text((sizePizza+crustPrice+beef+pineapple+delivery)*numberPizza)
+  $(".total").text((sizePizza+crustPrice+beacon+cheese+pineapple+delivery)*numberPizza);
 }
 
 if(mbeacon==80&&mpineapple==70)
 {
   $(".showBeef").hide()
   $(".titleBeef").hide()
-  $(".total").text((sizePizza+crustPrice+mbeacon+mpineapple+delivery)*numberPizza)
+  $(".total").text((sizePizza+crustPrice+mbeacon+mcheese+mpineapple+delivery)*numberPizza);
 }
 
 if(lbeacon==90&&lpineapple==80)
 {
   $(".showBeef").hide()
   $(".titleBeef").hide()
-  $(".total").text((sizePizza+crustPrice+lbeacon+lpineapple+delivery)*numberPizza)
+  $(".total").text((sizePizza+crustPrice+lbeacon+lcheese+lpineapple+delivery)*numberPizza);
 }
 
 
-
-
-
-
-
+if(beef==40 &&cheese)
+{
+  $(".showPineapple").hide()
+  $(".titlePineapple").hide()
+  $(".showBeacon").hide()
+  $(".titleBeacon").hide()
+  
+  $(".total").text((sizePizza+crustPrice+beef+cheese+delivery)*numberPizza);
+}
+if(pineapple==50 &&cheese)
+{
+  $(".showBeef").hide()
+  $(".titleBeef").hide()
+  $(".showBeacon").hide()
+  $(".titleBeacon").hide()
+  $(".total").text((sizePizza+crustPrice+pineapple+cheese+delivery)*numberPizza);
+}
+if(beacon==60 &&cheese)
+{
+  $(".showPineapple").hide()
+  $(".titlePineapple").hide()
+  $(".showBeef").hide()
+  $(".titleBeef").hide()
+  $(".total").text((sizePizza+crustPrice+beacon+cheese+delivery)*numberPizza);}
 
 
 
